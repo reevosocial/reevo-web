d72b15ada737bb472799b5313336416b7bc63138
Yaco <franco@redesdepares.org>
2015-01-21 13:49:31 -0300

    cambio en core para que se muestren todas las actividades por defecto

--- www/equipo/lib/redmine.rb
+++ www/equipo/lib/redmine.rb
@@ -256,9 +256,9 @@ Redmine::Activity.map do |activity|
   activity.register :news
   activity.register :documents, :class_name => %w(Document Attachment)
   activity.register :files, :class_name => 'Attachment'
-  activity.register :wiki_edits, :class_name => 'WikiContent::Version', :default => false
-  activity.register :messages, :default => false
-  activity.register :time_entries, :default => false
+  activity.register :wiki_edits, :class_name => 'WikiContent::Version', :default => true
+  activity.register :messages, :default => true
+  activity.register :time_entries, :default => true
 end
 
 Redmine::Search.map do |search|
