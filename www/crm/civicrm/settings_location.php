1948985dfa63c479129062148fb07a754f6e187b
Yaco <franco@redesdepares.org>
2015-02-23 20:40:24 -0300

    agrega archivo para fijar la ruta de crm

---
+++ www/crm/civicrm/settings_location.php
@@ -0,0 +1,12 @@
+
+<?php
+  
+// the directory name where your civicrm.settings.php file is located
+define( 'CIVICRM_CONFDIR', '/srv/reevo-web/www/crm' );
+ 
+// the directory name where you CMS is installed
+// this needs to be defined when CiviCRM is not installed
+// as a subdirectory of the CMS. This is currently used only
+// in WordPress
+// http://codex.wordpress.org/Editing_wp-config.php#Moving_wp-content
+define( 'CIVICRM_CMSDIR', '/srv/reevo-web/www/blog');
