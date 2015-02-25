74edb20f855b9b7e47ae5e0d623f07ae091bba9a
Yaco <franco@redesdepares.org>
2015-02-25 15:29:44 -0300

    oculta los errores

--- www/crm/civicrm/settings_location.php
+++ www/crm/civicrm/settings_location.php
@@ -1,6 +1,10 @@
 
 <?php
-  
+
+// Disable errors show
+error_reporting(0);
+ini_set('display_errors', 0);
+
 // the directory name where your civicrm.settings.php file is located
 define( 'CIVICRM_CONFDIR', '/srv/reevo-web/www/crm' );
  
