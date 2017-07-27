dfb95151332d4cba27f6d8a17816b68950df6d55
Yaco <franco@reevo.org>
2017-07-27 10:48:53 -0300

    agrega plugin upload_users a elgg

---
+++ lib/red-plugins/upload_users/views/default/admin/users/upload.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Upload users admin interface
+ *
+ * @package upload_users
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jaakko Naakka / Mediamaisteri Group
+ * @author Ismayil Khayredinov / Arck Interactive
+ * @copyright Mediamaisteri Group 2009
+ * @copyright ArckInteractive 2013
+ * @link http://www.mediamaisteri.com/
+ * @link http://arckinteractive.com/
+ */
+elgg_load_css('upload_users.css');
+elgg_require_js('upload_users');
+
+$guid = get_input('guid');
+$file = get_entity($guid);
+
+switch ($file->status) {
+	default :
+		$body = elgg_view('upload_users/incomplete'); // Show unfinished user imports
+		$body .= elgg_view('upload_users/upload'); // Show upload form
+		$body .= elgg_view('upload_users/instructions'); // Show upload instructions
+		break;
+
+	case 'uploaded' :
+		$body = elgg_view('upload_users/map_fields');
+		break;
+
+	case 'premapped' :
+		$body = elgg_view('upload_users/map_required_fields');
+		break;
+
+	case 'mapped' :
+	case 'imported' :
+		$body = elgg_view('upload_users/import');
+		break;
+
+}
+
+echo $body;
