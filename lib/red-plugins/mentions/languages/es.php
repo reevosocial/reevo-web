c5fa3bf5271c3cecc6ee35b2a336d086b230b687
Yaco <franco@reevo.org>
2017-09-21 16:50:50 -0300

    agrega plugin mentions a elgg

---
+++ lib/red-plugins/mentions/languages/es.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+	'mentions:notification:subject' => '%s te mencionó en %s',
+	'mentions:notification:body' => '%s te mencionó en %s.
+
+Para ver la publicación completa, haz click en el siguiente enlace:
+%s
+',
+	'mentions:notification_types:object:blog' => 'una publicación en el blog',
+	'mentions:notification_types:object:bookmarks' => 'un marcador',
+	'mentions:notification_types:object:groupforumtopic' => 'una discusión en un grupo',
+	'mentions:notification_types:object:discussion_reply' => 'una respuesta en un grupo',
+	'mentions:notification_types:object:thewire' => 'un telegrama',
+	'mentions:notification_types:object:comment' => 'un comentario',
+	'mentions:settings:send_notification' => 'Recibir notificaciones cuando alguien me mencione (al escribir tu @nombre_de_usuario)',
+
+	// admin
+	'mentions:fancy_links' => 'Inclur avatar del usuario en las @menciones?',
+
+	'mentions:settings:failed' => 'No se pudieron guardar la configuración.'
+);
