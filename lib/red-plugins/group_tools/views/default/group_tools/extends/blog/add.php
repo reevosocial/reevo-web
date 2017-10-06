fb1ec33899c0562b5cd1421e09380d8f3ecda4ff
Yaco <franco@reevo.org>
2017-10-06 11:55:30 -0300

    corrige error que no permitia publicar en blog personal

--- lib/red-plugins/group_tools/views/default/group_tools/extends/blog/add.php
+++ lib/red-plugins/group_tools/views/default/group_tools/extends/blog/add.php
@@ -1,7 +1,10 @@
 <?php
 $guid = elgg_extract('guid', $vars);
-if (group_tools_blogadmin_access($guid) == 0) {
-  register_error(elgg_echo('group_tools:group:blogadmin:error'));
-  forward(REFERER);
+$owner = elgg_get_page_owner_entity();
+if ($owner->type == 'group') {
+  if (group_tools_blogadmin_access($guid) == 0) {
+    register_error(elgg_echo('group_tools:group:blogadmin:error'));
+    forward(REFERER);
+  }
 }
 ?>
