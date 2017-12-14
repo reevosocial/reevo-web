9ffa0cc69a3dfa5b8e630e37a75fa182e14692ef
Yaco <franco@reevo.org>
2017-12-14 20:00:11 -0300

    corrige url de ogimage para eventos

--- lib/red-plugins/reevo_custom/views/default/event_manager/event/view.php
+++ lib/red-plugins/reevo_custom/views/default/event_manager/event/view.php
@@ -4,20 +4,15 @@ elgg_register_plugin_hook_handler('header', 'opengraph', function ($hook, $handl
 	elgg_entity_gatekeeper($guid, 'object', 'event');
 	$event = get_entity($guid);
 	$baseurl = rtrim(elgg_get_site_url(), "/");
-	$event_banner_url = '';
-	if ($event->hasIcon('event_banner')) {
-		$event_banner_url = $event->getIconURL('event_banner');
-	} elseif ($event->hasIcon('master')) {
-		$event_banner_url = $event->getIconURL('master');
-	}
 
-	// fix feo para obtener una URL directa al archivo y que lo lea FB
-	$event_banner_url_array = explode('/',strrev($event_banner_url));
-	$event_banner_url = $baseurl . '/files/' . strrev($event_banner_url_array[2]) . '/'. strrev($event_banner_url_array[1]) . '/' . strrev($event_banner_url_array[0]);
+	if ($event->hasIcon('master')) {
+		$icon = $event->getIcon('master');
+		$iconfinal = elgg_get_inline_url($icon, false);
+		$return['og:image'] = $iconfinal;
+	}
 
 	if (preg_match('/'.str_replace('/','\\/',elgg_get_site_url()).'event/', $params['url'])) {
 			$return['og:description'] = strip_tags($event->shortdescription);
-			$return['og:image'] = $event_banner_url;
 			return $return;
 	}
 });
