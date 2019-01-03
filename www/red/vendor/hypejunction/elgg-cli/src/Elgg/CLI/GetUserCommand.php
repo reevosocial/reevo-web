cb6691ab179d520184c4d91399868d68f8bec701
Yaco <franco@reevo.org>
2019-01-03 17:19:31 -0300

    agrega funcion para listar usuarios por username o por email

---
+++ www/red/vendor/hypejunction/elgg-cli/src/Elgg/CLI/GetUserCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Elgg\CLI;
+
+use Elgg\Application;
+use Exception;
+use RegistrationException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+error_reporting(0);
+
+/**
+ * user:add CLI command
+ */
+class GetUserCommand extends Command {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function configure() {
+		$this->setName('user:get')
+				->setDescription('Check for user account')
+				->addOption('email', null, InputOption::VALUE_OPTIONAL, 'Search keyword')
+				->addOption('username', null, InputOption::VALUE_OPTIONAL, 'Search keyword')
+				->addOption('as', null, InputOption::VALUE_OPTIONAL, 'Username of the user to login');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function handle() {
+
+		$admin = get_user('55');
+		$login = login($admin);
+
+		$username = $this->option('username');
+		$email = $this->option('email');
+
+		if ($this->option('username')) {
+			$user = get_user_by_username($username);
+			if ($user) {
+				// print_r($user);
+			} else {
+				exit();
+			}
+		}
+
+		if ($this->option('email')) {
+			$user = get_user_by_email($email);
+			if ($user) {
+				$user = $user[0];
+			} else {
+				exit();
+			}
+		}
+		$export['username'] = $user->username;
+		$export['email'] = $user->email;
+		$export['name'] = $user->name;
+		$export['description'] = $user->description;
+		$export['briefdescription'] = $user->briefdescription;
+		$export['interests'] = $user->interests;
+		$export['website'] = $user->website;
+		$export['guid'] = $user->guid;
+		$export['icon'] = $user->getIconURL('master');
+		// echo $user->description;
+
+		print_r($export);
+		// print_r($login);
+
+		// $guid = register_user($username, $password, $name, $email);
+		// $user = get_user($guid);
+		// print_r($user);
+
+
+	}
+
+}
